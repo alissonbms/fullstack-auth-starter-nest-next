5aaad9d6272e6c1df842d849df99c68de87aeb61
alissonbms <alissonnewdev@gmail.com>
2025-04-19 14:48:12 -0300

    refactor: improve TokenService with unified logic and proper expiresIn typing using ms.StringValue

--- server/src/auth/token.service.ts
+++ server/src/auth/token.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { CustomConfigService } from "src/config/custom-config.service";
 import { TokenPayload } from "./interfaces/token-payload.interface";
+import * as ms from "ms";
 
+type TokenWithExpiration = {
+  token: string;
+  expiresIn: ms.StringValue;
+};
 @Injectable()
 export class TokenService {
   constructor(
@@ -14,46 +19,34 @@ export class TokenService {
     payload: TokenPayload,
     secret: string,
     expiresIn: string,
-  ): { token: string; expiresIn: string } {
+  ): TokenWithExpiration {
     const token = this.jwtService.sign(payload, { secret, expiresIn });
-    return { token, expiresIn };
+    return { token, expiresIn: expiresIn as ms.StringValue };
   }
 
   verifyToken(token: string, secret: string): { sub: string } {
     return this.jwtService.verify(token, { secret });
   }
 
-  generateAccessToken(payload: TokenPayload): {
-    token: string;
-    expiresIn: string;
-  } {
+  generateAccessToken(payload: TokenPayload): TokenWithExpiration {
     const secret = this.customConfigService.getJwtAccessSecret();
     const expiresIn = this.customConfigService.getJwtAccessExpiration();
     return this.generateToken(payload, secret, expiresIn);
   }
 
-  generateConfirmationToken(payload: TokenPayload): {
-    token: string;
-    expiresIn: string;
-  } {
+  generateConfirmationToken(payload: TokenPayload): TokenWithExpiration {
     const secret = this.customConfigService.getJwtConfirmationSecret();
     const expiresIn = "1h";
     return this.generateToken(payload, secret, expiresIn);
   }
 
-  generateChangeEmailToken(payload: TokenPayload): {
-    token: string;
-    expiresIn: string;
-  } {
+  generateChangeEmailToken(payload: TokenPayload): TokenWithExpiration {
     const secret = this.customConfigService.getJwtChangeEmailSecret();
     const expiresIn = "1h";
     return this.generateToken(payload, secret, expiresIn);
   }
 
-  generateResetPasswordToken(payload: TokenPayload): {
-    token: string;
-    expiresIn: string;
-  } {
+  generateResetPasswordToken(payload: TokenPayload): TokenWithExpiration {
     const secret = this.customConfigService.getJwtResetPasswordSecret();
     const expiresIn = "1h";
     return this.generateToken(payload, secret, expiresIn);
