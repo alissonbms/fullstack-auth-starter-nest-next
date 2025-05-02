02d067d11fba74401d6af4c995460726c1f261c4
alissonbms <alissonnewdev@gmail.com>
2025-05-02 08:57:11 -0300

    feat: create responsive Container component with optional muted background

---
+++ client/components/container.tsx
@@ -0,0 +1,18 @@
+interface ContainerProps {
+  children: React.ReactNode;
+  muted: boolean;
+}
+
+const Container = ({ children, muted }: ContainerProps) => {
+  return (
+    <div
+      className={`flex h-[100vh] w-[100vw] items-center justify-center ${
+        muted ? "bg-muted" : ""
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
