dc2a034346b36f812c54c8c86bf1b30e4b3cff3a
alissonbms <alissonnewdev@gmail.com>
2025-05-09 15:58:49 -0300

    feat: add confirmPassword field and matching validation to resetPassword schema; document same previous addition to signup schema

--- client/schemas/reset-password.ts
+++ client/schemas/reset-password.ts
@@ -1,12 +1,21 @@
 import { z } from "zod";
 
-export const ResetPasswordSchema = z.object({
-  password: z
-    .string()
-    .trim()
-    .min(1, "Password must not be empty.")
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/,
-      "Password must be at least 8 characters long, including numbers, upper and lower case letters, and symbols.",
-    ),
-});
+export const ResetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .trim()
+      .min(1, "Password must not be empty.")
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$/,
+        "Password must be at least 8 characters long, including numbers, upper and lower case letters, and symbols.",
+      ),
+    confirmPassword: z
+      .string()
+      .trim()
+      .min(1, "Password confirmation must not be empty."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match.",
+    path: ["confirmPassword"],
+  });
